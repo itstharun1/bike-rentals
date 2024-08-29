52760b4bb4458133cc07885ccdf34110a0e75ada
itstharun1 <tharunganjai@gmail.com>
2024-08-29 12:05:49 +0530

    main
    changes

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,13 @@ const app = express();
 
 const PORT = 8000;
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 app.use('/',router);
 
 app.use(express.json())
