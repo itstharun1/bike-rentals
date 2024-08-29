f0b291b892ea80da462f1b08c6db4476d12b5414
itstharun1 <tharunganjai@gmail.com>
2024-08-29 12:50:25 +0530

    main

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,6 @@ import dotenv from 'dotenv'
 import router from './routes/routes.js';
 import bodyParser from "body-parser"
 import cors from 'cors'
-import path from 'path';
-import { dirname } from 'path';
-
 
 
 
@@ -28,10 +25,7 @@ app.use('/',router);
 app.use(express.json())
 
 
-app.get('/',(req,res)=>{
-    app.use(express.static(path.resolve(__dirname,"front-end","dist")));
-    res.sendFile(path.resolve(__dirname,"front-end","dist","index.html"));
-})
+
 
 app.listen(PORT,()=>console.log("server is running.."));
 
